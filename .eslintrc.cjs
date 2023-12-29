454648145b2a79d846c42472e4caf616bd6dcba6
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2023-12-29 17:40:59 +0700

    disable eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,8 @@ const config = {
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/no-unsafe-return": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
 
     "@typescript-eslint/consistent-type-imports": [
       "warn",
