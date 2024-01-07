2f1ec69973761a2f9c457da3c78107d3319e7ca1
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-01-07 14:44:23 +0700

    use trigger.dev openai integration

--- src/jobs/module-generator.ts
+++ src/jobs/module-generator.ts
@@ -1,6 +1,5 @@
-import openai from "@/libs/openai";
 import duckduckgo from "@/libs/duckduckgo";
-import { trigger as client } from "@/libs/trigger";
+import { trigger as client, openai } from "@/libs/trigger";
 import { db } from "@/server/db";
 import { moduleGeneratorSchema } from "./schema";
 import { eventTrigger } from "@trigger.dev/sdk";
@@ -14,41 +13,38 @@ client.defineJob({
     name: "module.generator",
     schema: moduleGeneratorSchema,
   }),
+  integrations: {
+    openai,
+  },
   run: async ({ modules, transcript, subject }, io) => {
     for (const { id, title, overview } of modules) {
       await io.logger.info(`[module-generator] START: ${id}-${title}`);
 
-      const { reading } = await io.runTask(
+      const content = `As a part of my "${subject}" subject learning especially with a topic called "${title}" with a general overview "${overview}". I want you to generate a reading material with formal writting style for my learning. 
+        
+      Give me the reading material as complete and concise as possible. You can divide the reading material into sequential sections if needed. I have a transcript also that you can use this additional context. Here is the transcript, "${transcript}". 
+      
+      Return the reading material without any starting sentence "Based on this transcription..."  and without any ending sentence something like "If you have any further questions or need clarification on any topic, feel free to ask. Happy learning!". 
+
+      If sections are being used, please end the reading material section with "Conclusion" section as a summarization of all the sections above. Don't use section numbering. Use section title only for each section. Use H2 for section title heading.
+      
+      Just return the reading material only in markdown style. No need to give the markdown a title. Start the reading material with a sub-heading of the first section title.`;
+
+      const response = await io.openai.chat.completions.create(
         `module-generator-${id}`,
-        async () => {
-          const content = `As a part of my "${subject}" subject learning especially with a topic called "${title}" with a general overview "${overview}". I want you to generate a reading material with formal writting style for my learning. 
-        
-        Give me the reading material as complete and concise as possible. You can divide the reading material into sequential sections if needed. I have a transcript also that you can use this additional context. Here is the transcript, "${transcript}". 
-        
-        Return the reading material without any starting sentence "Based on this transcription..."  and without any ending sentence something like "If you have any further questions or need clarification on any topic, feel free to ask. Happy learning!". 
-
-        If sections are being used, please end the reading material section with "Conclusion" section as a summarization of all the sections above. Don't use section numbering. Use section title only for each section. Use H2 for section title heading.
-        
-        Just return the reading material only in markdown style. No need to give the markdown a title. Start the reading material with a sub-heading of the first section title.`;
-
-          const request = await openai.chat.completions.create({
-            messages: [
-              {
-                role: "system",
-                content: `You're a very smart teaching assistant that will teach me about "${subject}" subject`,
-              },
-              {
-                role: "user",
-                content,
-              },
-            ],
-            temperature: 0.1,
-            model: "gpt-3.5-turbo-16k-0613",
-          });
-
-          const response = request.choices[0]?.message.content;
-
-          return { reading: response };
+        {
+          messages: [
+            {
+              role: "system",
+              content: `You're a very smart teaching assistant that will teach me about "${subject}" subject`,
+            },
+            {
+              role: "user",
+              content,
+            },
+          ],
+          temperature: 0.1,
+          model: "gpt-3.5-turbo-16k-0613",
         },
       );
 
@@ -65,15 +61,13 @@ client.defineJob({
         return await db.module.update({
           where: { id },
           data: {
-            reading,
+            reading: response.choices[0]?.message.content,
             references: JSON.stringify(references),
           },
         });
       });
 
       await io.logger.info(`[module-generator] DONE: ${id}-${title}`);
-      /* 
-      return { reading, references }; */
     }
 
     return { modules, transcript, subject };
--- src/libs/trigger.ts
+++ src/libs/trigger.ts
@@ -1,8 +1,14 @@
 import { env } from "@/env";
 import { TriggerClient } from "@trigger.dev/sdk";
+import { OpenAI } from "@trigger.dev/openai";
 
 export const trigger = new TriggerClient({
   id: env.TRIGGER_ID,
   apiKey: env.TRIGGER_API_KEY,
   apiUrl: env.TRIGGER_API_URL,
+});
+
+export const openai = new OpenAI({
+  id: "openai",
+  apiKey: env.OPENAI_API_KEY,
 });
