4adabb13fd119eab5ea92079e9b6fdaa50702e3a
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-01-10 17:02:11 +0700

    fix

--- src/pages/api/trigger.ts
+++ src/pages/api/trigger.ts
@@ -4,12 +4,15 @@ import { createPagesRoute } from "@trigger.dev/nextjs";
 import "@/jobs";
 
 //uncomment this to set a higher max duration (it must be inside your plan limits). Full docs: https://vercel.com/docs/functions/serverless-functions/runtimes#max-duration
+/* export const config = {
+  maxDuration: 300,
+}; */
 
 //this route is used to send and receive data with Trigger.dev
 const { handler, config: triggerConfig } = createPagesRoute(trigger);
 export const config = {
   maxDuration: 300,
-  ...triggerConfig,
+  ...triggerConfig.api,
 };
 
 export default handler;
