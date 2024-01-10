3f01cf244bcbef941b52ae1011e20a203cac384c
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-01-10 16:20:43 +0700

    add focus, blur

--- src/common/hooks/ui/useDocumentVisibility.ts
+++ src/common/hooks/ui/useDocumentVisibility.ts
@@ -3,23 +3,37 @@ import { useEffect } from "react";
 const useDocumentVisibility = (props: {
   onVisible?: () => Promise<void>;
   onHidden?: () => Promise<void>;
-  deps?: Array<unknown>;
+  deps?: Array<never>;
 }) => {
   useEffect(() => {
+    const onFocus = () => {
+      void props.onVisible?.();
+    };
+    const onBlur = () => {
+      void props.onHidden?.();
+    };
     const handleVisibilityChange = () => {
       if (document.hidden) {
-        void props.onHidden?.();
+        onBlur();
       } else {
-        void props.onVisible?.();
+        onFocus();
       }
     };
 
     if ("hidden" in document) {
       document.addEventListener("visibilitychange", handleVisibilityChange);
+      props.onVisible ? document.addEventListener("focus", onFocus) : undefined;
+      props.onHidden ? document.addEventListener("blur", onFocus) : undefined;
     }
 
     return () => {
       document.removeEventListener("visibilitychange", handleVisibilityChange);
+      props.onVisible
+        ? document.removeEventListener("focus", onFocus)
+        : undefined;
+      props.onHidden
+        ? document.removeEventListener("blur", onFocus)
+        : undefined;
     };
   }, [props]);
 };
