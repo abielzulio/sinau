bf0a26d2464b21dcb1a1182a54bf35b9e64c4131
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-01-23 10:50:16 +0700

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,5 +5,5 @@ export function middleware() {
 }
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc|trigger)(.*)"],
 };
