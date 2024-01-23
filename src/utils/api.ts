095697c0d57adccd1cc7297685f9ebc97d6d6e2f
Abiel Zulio M <7030944+abielzulio@users.noreply.github.com>
2024-01-23 10:50:22 +0700

    omit connection

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -43,13 +43,24 @@ export const api = createTRPCNext<AppRouter>({
         httpBatchLink({
           url: `${getBaseUrl()}/api/trpc`,
           headers() {
-            if (!ctx?.req?.headers) {
-              return {};
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so that you can pass through things like cookies when we're server-side
+              // rendering
+              // If you're using Node 18, omit the "connection" header
+              const {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                connection: _connection,
+                ...headers
+              } = ctx.req.headers;
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                "x-ssr": "1",
+              };
             }
-            // To use SSR properly, you need to forward client headers to the server
-            // This is so you can pass through things like cookies when we're server-side rendering
             return {
-              cookie: ctx.req.headers.cookie,
+              cookie: ctx?.req?.headers.cookie,
             };
           },
         }),
